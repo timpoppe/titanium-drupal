e007b9c564dfa4ff367d6300cf9ec4d91d2acbdf
Joshua Schroeder <josh.schroeder@uleth.ca>
2010-09-10 16:29:13 -0600

    Adding config file and populating table view with content from Drupal view

---
+++ config.js
@@ -0,0 +1,2 @@
+// Set this to the base path of your Drupal installation (no trailing slash) 
+var SITE_PATH = 'http://services:8082';
--- teams_view.js
+++ teams_view.js
@@ -1,19 +1,43 @@
+// Include our configuration settings
+Ti.include('config.js');
+
 win = Ti.UI.currentWindow;
 
-var tableview = Titanium.UI.createTableView({
-  data:[{title:'Item 1'}, {title:'Item 2'}, {title:'Item 3'}]
-});
+// URL of the web service endpoint
+var url = SITE_PATH + '/services/json';
 
-// Add an event listener for taps on the row items
-tableview.addEventListener('click',function(e) { 
-  var teamWindow = Titanium.UI.createWindow({
-    url:'team.js',
-    backgroundColor:'#fff',
-    title:e.rowData.title,
-    nid:e.rowData.nid
-  });
-  Titanium.UI.currentTab.open(teamWindow,{animated:true});
-});
+var view = new Object;
+view.method = 'views.get';
+view.view_name = 'teams';
 
-// add table view to the window
-win.add(tableview);
+// Add an on-focus event listener so that we can reload data on the fly
+win.addEventListener('focus', function() {
+  var xhr = Titanium.Network.createHTTPClient();
+  xhr.open("POST",url);
+
+  // sending xhr call for view
+  xhr.send({data: JSON.stringify(view)});
+
+  // Once the data is loaded, we can do stuff with it
+  xhr.onload = function() {
+
+    // Parse JSON response into an object
+    var data = JSON.parse(this.responseText);
+
+    // custom drupal call stuff ends
+    var table_data = [];
+    var current_division = '';
+    for (var c=0;c<data.length;c++) {
+      // The data we care about: node title (team name), and node id
+      table_data[c] = {title:data[c].node_title, nid:data[c].nid, hasChild:true};
+    }
+
+    // create table view
+    var tableview = Titanium.UI.createTableView({
+      data:table_data
+    });
+
+    // add table view to the window
+    win.add(tableview);
+  }
+});
