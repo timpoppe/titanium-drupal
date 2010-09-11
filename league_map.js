d10b1f631181a12a35250af4ecb596ce832c5150
Joshua Schroeder <josh.schroeder@uleth.ca>
2010-09-10 23:43:00 -0600

    Adding team locations to league map

--- league_map.js
+++ league_map.js
@@ -1,11 +1,49 @@
+Ti.include('config.js');
+
 win = Ti.UI.currentWindow;
 
-var mapview = Titanium.Map.createView({
-	mapType: Titanium.Map.STANDARD_TYPE,
-	region: {latitude:49.666904, longitude:-112.805321, latitudeDelta:10, longitudeDelta:10},
-	animate:true,
-	regionFit:true,
-	userLocation:true
-});
+var url = SITE_PATH + '/services/json';
 
-win.add(mapview);
+var view = new Object;
+view.method = 'views.get';
+view.view_name = 'teams';
+
+var xhr = Titanium.Network.createHTTPClient();
+xhr.open("POST",url);
+
+// sending xhr call for view
+xhr.send({data: JSON.stringify(view)});
+
+xhr.onload = function() {
+  
+  Ti.API.info(this.responseText);
+  
+  var data = JSON.parse(this.responseText);
+
+
+  var mapview = Titanium.Map.createView({
+  	mapType: Titanium.Map.STANDARD_TYPE,
+  	region: {latitude:49.666904, longitude:-112.805321, latitudeDelta:10, longitudeDelta:10},
+  	animate:true,
+  	regionFit:true,
+  	userLocation:true
+  });
+
+  // custom drupal call stuff ends
+  var table_data = [];
+  var current_division = '';
+  for (var c=0;c<data.length;c++) {
+    
+    mapview.addAnnotation(Ti.Map.createAnnotation({
+      latitude:data[c].node_data_field_logo_field_latitude_value,
+      longitude:data[c].node_data_field_logo_field_longitude_value,
+      title:data[c].node_title,
+      subtitle: data[c].node_data_field_logo_field_arena_value,
+    	animate:false
+    }));
+    
+  }
+
+  win.add(mapview);
+  
+}
