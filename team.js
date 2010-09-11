1d7cf5e3969b2a0b5b0e4bdfe73c117d4842a78c
Joshua Schroeder <josh.schroeder@uleth.ca>
2010-09-10 23:36:18 -0600

    Adding nicely formatted team and arena names

--- team.js
+++ team.js
@@ -1,8 +1,47 @@
-win = Ti.UI.currentWindow;
+Ti.include('config.js');
 
-var teamName = Ti.UI.createLabel({
-	text: win.title,
-	color:'#000'
-});
+var win = Ti.UI.currentWindow;
 
-win.add(teamName);
+var url = SITE_PATH + '/services/json';
+
+var node = new Object;
+node.method = 'node.get';
+node.nid = win.nid;
+
+var xhr = Titanium.Network.createHTTPClient();
+xhr.open("POST",url);
+
+// sending xhr call for view
+xhr.send({data: JSON.stringify(node)});
+
+xhr.onload = function() {
+  
+  Ti.API.info(this.responseText);
+  
+  var data = JSON.parse(this.responseText);
+
+
+  var teamName = Ti.UI.createLabel({
+  	text: data.title,
+  	color:'#000',
+  	textAlign:'left',
+  	font:{fontSize:16, fontWeight:'bold'},
+  	top:32,
+  	left:100,
+  	height:18
+  });
+	
+	var arena = Ti.UI.createLabel({
+		text: data.field_arena[0].value,
+		color:'#000',
+		textAlign:'left',
+		font:{fontSize:14, fontWeight:'normal'},
+		top:51,
+		left:100,
+		height:16
+	});
+
+  win.add(teamName);
+  win.add(arena);
+
+}
